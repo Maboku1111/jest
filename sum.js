b85102b6416045b9bbbc9b7ba0e7ddcf13d3e217
EzrahCodes <112862267+Maboku1111@users.noreply.github.com>
2024-08-05 12:43:25 +0200

    Calculator application basic functions done and tested

--- sum.js
+++ sum.js
@@ -3,5 +3,20 @@ function sum(a, b) {
     return a + b;
 }
 
+// Create a function to subtract two numbers
+function subtract(a, b) {
+    return a - b;
+}
+
+// Create a function to multiply two numbers
+function multiply(a, b) {
+    return a * b;
+}
+
+// Create a function to divide two numbers
+function divide(a, b) {
+    return a / b;
+}
+
 // Be able to export this function into another file
-module.exports = sum;
+module.exports = { sum, subtract, multiply, divide };
--- sum.test.js
+++ sum.test.js
@@ -1,8 +1,26 @@
 // Import the function from the sum file 
-const sum = require('./sum');
+const { sum, subtract, multiply, divide } = require('./sum');
 
 
 // Test the function sum
 test('adds 3 + 4 to equal 7', () => {
     expect(sum(3, 4)).toBe(7);
 });
+
+
+// Test the function subtract
+test('subtracts 4 - 3 to equal 1', () => {
+    expect(subtract(4, 3)).toBe(1);
+})
+
+
+// Test the function multiply
+test('multiplies 3 * 4 to equal 12', () => {
+    expect(multiply(3, 4)).toBe(12);
+})
+
+
+// Test the function divide
+test('divides 12 / 3 to equal 4', () => {
+    expect(divide(12, 3)).toBe(4);
+})
